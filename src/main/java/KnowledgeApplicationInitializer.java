9496d7e633897608376a5067fe03f193ed991e34
polishchuk-tatyana <tatiana.polish23@gmail.com>
2023-03-20 20:12:45 +0200

    app initialization

--- src/main/java/KnowledgeApplicationInitializer.java
+++ src/main/java/KnowledgeApplicationInitializer.java
@@ -2,10 +2,12 @@ import org.springframework.web.WebApplicationInitializer;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import java.time.LocalDate;
 
 public class KnowledgeApplicationInitializer implements WebApplicationInitializer {
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         System.out.println("Hello!");
     }
+
 }
