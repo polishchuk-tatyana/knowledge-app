5c3b27cf746bf238529f5a1a9de5f5cb06b9c045
polishchuk-tatyana <tatiana.polish23@gmail.com>
2023-03-20 20:12:00 +0200

    services

--- src/main/java/com/knowledge/app/service/KnowledgePackageService.java
+++ src/main/java/com/knowledge/app/service/KnowledgePackageService.java
@@ -1,18 +1,52 @@
 package com.knowledge.app.service;
 
 import com.knowledge.app.domain.KnowledgePackage;
+import com.knowledge.app.repository.KPacSetAttachmentRepository;
 import com.knowledge.app.repository.KnowledgePackageRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-
+import com.knowledge.app.web.dto.CustomMapper;
+import com.knowledge.app.web.dto.FormDataKnowledgePackageDTO;
+import org.springframework.stereotype.Component;
 import java.util.List;
 
+@Component
 public class KnowledgePackageService {
 
-    @Autowired
-    private KnowledgePackageRepository knowledgePackageRepository;
+    private final KnowledgePackageRepository knowledgePackageRepository;
+    private final KPacSetAttachmentRepository kPacSetAttachmentRepository;
+    private CustomMapper mapper;
 
-    public List<KnowledgePackage> findAll(){
+    public KnowledgePackageService(
+            KnowledgePackageRepository knowledgePackageRepository,
+            KPacSetAttachmentRepository kPacSetAttachmentRepository,
+            CustomMapper mapper) {
+        this.knowledgePackageRepository = knowledgePackageRepository;
+        this.kPacSetAttachmentRepository = kPacSetAttachmentRepository;
+        this.mapper = mapper;
+    }
+
+    public List<KnowledgePackage> findAll() {
         return knowledgePackageRepository.findAll();
     }
 
+    public void save(FormDataKnowledgePackageDTO formDataKnowledgePackageDTO) {
+        KnowledgePackage knowledgePackage = mapper.toKnowledgePackage(formDataKnowledgePackageDTO);
+        knowledgePackageRepository.save(knowledgePackage);
+    }
+
+    public void saveAndAttachToSet(FormDataKnowledgePackageDTO formDataKnowledgePackageDTO) {
+        save(formDataKnowledgePackageDTO);
+        kPacSetAttachmentRepository.attach(
+                knowledgePackageRepository.getLastKnowledgePackageId(),
+                formDataKnowledgePackageDTO.getKnowledgePackageSetId()
+        );
+    }
+
+    public void delete(int knowledgePackageId) {
+        kPacSetAttachmentRepository.deleteKnowledgePackage(knowledgePackageId);
+        knowledgePackageRepository.delete(knowledgePackageId);
+    }
+
+    public List<KnowledgePackage> findAllByKnowledgePackageSetId(int id) {
+        return knowledgePackageRepository.findAllByKnowledgePackageSetId(id);
+    }
 }
--- src/main/java/com/knowledge/app/service/KnowledgePackageSetService.java
+++ src/main/java/com/knowledge/app/service/KnowledgePackageSetService.java
@@ -1,17 +1,37 @@
 package com.knowledge.app.service;
 
 import com.knowledge.app.domain.KnowledgePackageSet;
+import com.knowledge.app.repository.KPacSetAttachmentRepository;
 import com.knowledge.app.repository.KnowledgePackageSetRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class KnowledgePackageSetService {
 
-    @Autowired
-    private KnowledgePackageSetRepository knowledgePackageSetRepository;
+    private final KnowledgePackageSetRepository knowledgePackageSetRepository;
+    private final KPacSetAttachmentRepository kPacSetAttachmentRepository;
+
+    public KnowledgePackageSetService(
+            KnowledgePackageSetRepository knowledgePackageSetRepository,
+            KPacSetAttachmentRepository kPacSetAttachmentRepository) {
+        this.knowledgePackageSetRepository = knowledgePackageSetRepository;
+        this.kPacSetAttachmentRepository = kPacSetAttachmentRepository;
+    }
 
     public List<KnowledgePackageSet> findAll(){
         return knowledgePackageSetRepository.findAll();
+    }
+
+    public void save(KnowledgePackageSet knowledgePackageSet){
+        knowledgePackageSetRepository.save(knowledgePackageSet);
+    }
+
+    public void delete(int knowledgePackageSetId){
+        if(kPacSetAttachmentRepository.findAttachedKnowledgePackageSet(knowledgePackageSetId)){
+            kPacSetAttachmentRepository.deleteKnowledgePackageSet(knowledgePackageSetId);
+        }
+        knowledgePackageSetRepository.delete(knowledgePackageSetId);
     }
 }
