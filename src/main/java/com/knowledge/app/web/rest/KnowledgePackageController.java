fde855283ee8515c0e0181a3411a1d23dc664a37
polishchuk-tatyana <tatiana.polish23@gmail.com>
2023-03-20 20:12:17 +0200

    cpntrollers

--- src/main/java/com/knowledge/app/web/rest/KnowledgePackageController.java
+++ src/main/java/com/knowledge/app/web/rest/KnowledgePackageController.java
@@ -1,32 +1,55 @@
 package com.knowledge.app.web.rest;
 
+import com.knowledge.app.domain.KnowledgePackage;
+import com.knowledge.app.domain.KnowledgePackageSet;
+import com.knowledge.app.service.KnowledgePackageService;
+import com.knowledge.app.service.KnowledgePackageSetService;
+import com.knowledge.app.web.dto.FormDataKnowledgePackageDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import java.util.List;
 
 @Controller
 @RequestMapping("/kpacs")
 public class KnowledgePackageController {
 
-    @GetMapping
-    public ModelAndView getAllKnowledgePackages(
-            Model model,
-            @RequestParam(name = "name", required = false, defaultValue = "tttttt") String name
-//            @WebFilter() Pageable pageable
-    ) {
-        model.addAttribute("m", name);
-        return null;
-//                new ModelAndView("knowledge-packages", model);
+    private final KnowledgePackageService knowledgePackageService;
+    private final KnowledgePackageSetService knowledgePackageSetService;
+
+    public KnowledgePackageController(
+            KnowledgePackageService knowledgePackageService,
+            KnowledgePackageSetService knowledgePackageSetService) {
+        this.knowledgePackageService = knowledgePackageService;
+        this.knowledgePackageSetService = knowledgePackageSetService;
     }
 
-    @PostMapping
-    public ModelAndView createKnowledgePackage() {
-        return null;
+    @GetMapping
+    public String getAllKnowledgePackages(Model model) {
+        List<KnowledgePackage> knowledgePackages = knowledgePackageService.findAll();
+        model.addAttribute("knowledgePackages", knowledgePackages);
+        return "knowledge-package/knowledge-packages";
+    }
+
+    @GetMapping("/showAddKnowledgePackageForm")
+    public String showFormForAdd(Model model) {
+        FormDataKnowledgePackageDTO formDataKnowledgePackageDTO = new FormDataKnowledgePackageDTO();
+        List<KnowledgePackageSet> knowledgePackageSets = knowledgePackageSetService.findAll();
+        model.addAttribute("formDataKnowledgePackageDTO", formDataKnowledgePackageDTO);
+        model.addAttribute("knowledgePackageSets", knowledgePackageSets);
+        return "knowledge-package/knowledge-package-form";
+    }
+
+    @PostMapping("/addKnowledgePackage")
+    public String addKnowledgePackage(
+            @ModelAttribute("formDataKnowledgePackageDTO") FormDataKnowledgePackageDTO formDataKnowledgePackageDTO) {
+        knowledgePackageService.saveAndAttachToSet(formDataKnowledgePackageDTO);
+        return "redirect:/kpacs";
     }
 
     @DeleteMapping("/{id}")
-    public ModelAndView delete(@PathVariable("id") Long id) {
-        return null;
+    public String deleteKnowledgePackage(@PathVariable("id") int knowledgePackageId) {
+        knowledgePackageService.delete(knowledgePackageId);
+        return "redirect:/kpacs";
     }
 }
--- src/main/java/com/knowledge/app/web/rest/KnowledgePackageSetController.java
+++ src/main/java/com/knowledge/app/web/rest/KnowledgePackageSetController.java
@@ -1,37 +1,60 @@
 package com.knowledge.app.web.rest;
 
+import com.knowledge.app.domain.KnowledgePackage;
+import com.knowledge.app.domain.KnowledgePackageSet;
+import com.knowledge.app.service.KnowledgePackageService;
+import com.knowledge.app.service.KnowledgePackageSetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import java.util.List;
 
 @Controller
 @RequestMapping("/sets")
 public class KnowledgePackageSetController {
 
+    private final KnowledgePackageSetService knowledgePackageSetService;
+    private final KnowledgePackageService knowledgePackageService;
+
+    public KnowledgePackageSetController(
+            KnowledgePackageSetService knowledgePackageSetService,
+            KnowledgePackageService knowledgePackageService) {
+        this.knowledgePackageSetService = knowledgePackageSetService;
+        this.knowledgePackageService = knowledgePackageService;
+    }
+
     @GetMapping
-    public ModelAndView getAllKnowledgePackageSets()
-    {
-        return null;
+    public String getAllKnowledgePackageSets(Model model) {
+        List<KnowledgePackageSet> knowledgePackageSets = knowledgePackageSetService.findAll();
+        model.addAttribute("knowledgePackageSets", knowledgePackageSets);
+        return "knowledge-package-set/knowledge-package-sets";
     }
 
-    @PostMapping
-    public ModelAndView createKnowledgePackageSet()
-    {
-        return null;
+    @GetMapping("/showAddKnowledgePackageSetForm")
+    public String showFormForAdd(Model model) {
+        KnowledgePackageSet knowledgePackageSet = new KnowledgePackageSet();
+        model.addAttribute("knowledgePackageSet", knowledgePackageSet);
+        return "knowledge-package-set/knowledge-package-set-form";
     }
 
-    @DeleteMapping
-    public ModelAndView deleteKnowledgePackageSet()
-    {
-        return null;
+    @PostMapping("/addKnowledgePackageSet")
+    public String addKnowledgePackageSet(
+            @ModelAttribute("knowledgePackageSet") KnowledgePackageSet knowledgePackageSet) {
+        knowledgePackageSetService.save(knowledgePackageSet);
+        return "redirect:/sets";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteKnowledgePackageSet(@PathVariable("id") int knowledgePackageSetId) {
+        knowledgePackageSetService.delete(knowledgePackageSetId);
+        return "redirect:/sets";
     }
 
     @GetMapping("/set/{id}")
-    public ModelAndView getAllKnowledgePackagesOfSet(
-            @PathVariable("id") Long id)
-    {
-//        knowledgepackages.stream().filter(kpac -> kpack.getId() == id).findAny().orElse(null);
-        return null;
+    public String getAllKnowledgePackagesOfSet(@PathVariable("id") int id, Model model) {
+        List<KnowledgePackage> knowledgePackagesOfSet = knowledgePackageService.findAllByKnowledgePackageSetId(id);
+//        knowledgePackagesOfSet.stream().filter(kpac -> kpack.getId() == id).findAny().orElse(null);
+        model.addAttribute("knowledgePackagesOfSet", knowledgePackagesOfSet);
+        return "knowledge-package-set/knowledge-packages-of-set";
     }
-
 }
